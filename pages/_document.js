0ff30ce4d871542782fa5076bdb162fbfd846a3e
yoanpi <52133759+yoanpi@users.noreply.github.com>
2022-06-16 15:18:02 +0200

    Ajoute le tracking avec plausible (#19)
    Co-authored-by: Yoan Piton <yoan@piton.local>

--- pages/_document.js
+++ pages/_document.js
@@ -5,11 +5,12 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <title>Simulateur Collectif Nos retraites</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab&family=Roboto:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&display=swap" rel="stylesheet" />
+        <script defer data-domain="nosretraites-simulateur-cas-types.netlify.app" src="https://plausible.io/js/plausible.js"></script>
       </Head>
+        <title>Simulateur Collectif Nos retraites</title>
       <body>
         <header className="globalHeader">
           <h1 className='title'>Simulateur de retraite</h1>
