fea1b3062aa033fffb416f6e19c36b055c8e207f
Yoan Piton <yoan@piton.local>
2022-06-16 12:04:05 +0200

    Supprime l'affichage des enfants pour les parents non impactés

--- components/ProfileCard.js
+++ components/ProfileCard.js
@@ -17,10 +17,10 @@ export default function ProfileCard({ selectedName, selectedPicto, gender, birth
                 <img src={selectedPicto} width={"150px"} />
             </div>
             <div className={styles.InformationBox}>
-                <span>Votre situation ressemble à celle de :</span>
+                <span>Votre situation ressemble à celle de :</span>
                 <span className={styles.Name}>{selectedName}</span>
-                <span>Né{gender !== "1" ? "e" : ""} en {birthDate} {parseInt(numberOfChildren) !== 0 ? parseInt(numberOfChildren) === 1 ? "- 1 enfant" : "- " + parseInt(numberOfChildren) + " enfants" : ""}</span>
-                <span>Durée requise : {yearsWithBase} annuités</span>
+                <span>Né{gender !== "1" ? "e" : ""} en {birthDate} {gender == "1" ? "" : parseInt(numberOfChildren) !== 0 ? parseInt(numberOfChildren) === 1 ? "- 1 enfant" : "- " + parseInt(numberOfChildren) + " enfants" : ""}</span>
+                <span>Durée requise : {yearsWithBase} annuités</span>
                 <span>Début de carrière à {careerStartAge} ans</span>
                 <Link href={{ pathname: '/', query: { birthDate, careerStartAge, gender, numberOfChildren } }}>
                     <a className="inlineButton">Retourner au formulaire</a>
