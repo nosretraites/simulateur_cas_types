a6a65c648f13d9257a677d589f080dd7126075c9
Yoan Piton <yoan@piton.local>
2022-06-16 11:54:30 +0200

    Corrige l'accord de né.e

--- components/ProfileCard.js
+++ components/ProfileCard.js
@@ -19,7 +19,7 @@ export default function ProfileCard({ selectedName, selectedPicto, gender, birth
             <div className={styles.InformationBox}>
                 <span>Votre situation ressemble à celle de :</span>
                 <span className={styles.Name}>{selectedName}</span>
-                <span>Né{gender !== 1 ? "e" : ""} en {birthDate} {parseInt(numberOfChildren) !== 0 ? parseInt(numberOfChildren) === 1 ? "- 1 enfant" : "- " + parseInt(numberOfChildren) + " enfants" : ""}</span>
+                <span>Né{gender !== "1" ? "e" : ""} en {birthDate} {parseInt(numberOfChildren) !== 0 ? parseInt(numberOfChildren) === 1 ? "- 1 enfant" : "- " + parseInt(numberOfChildren) + " enfants" : ""}</span>
                 <span>Durée requise : {yearsWithBase} annuités</span>
                 <span>Début de carrière à {careerStartAge} ans</span>
                 <Link href={{ pathname: '/', query: { birthDate, careerStartAge, gender, numberOfChildren } }}>
