d8fbee768d900fa0c6655fdde4e7754f4b9117dd
MEVEN <meven.ameline@gmail.com>
2023-01-10 22:29:50 +0100

    Ajout hébergeur mentions légales

--- pages/legals.js
+++ pages/legals.js
@@ -8,8 +8,14 @@ export default function legals() {
       <div className={styles.informationPage}>
         <h2>Mentions légales</h2>
         <p>Ce site est édité par le collectif Nos retraites.</p>
-        <p>Ce site est hébergé par /Hébergeur/ dans /région/ </p>
+        
         <p>Le site ne recueille pas d’informations personnelles, et n’est pas assujetti à déclaration à la CNIL. Les informations renseignées dans le formulaire ne sont utilisées que pour simuler un profil de retraites, elles ne sont en aucun cas stockées.</p>
+        <p>Ce site est hébergé par :</p>
+        <p> Netlify</p>
+        <p>610 22nd Street, Suite 315,</p>
+        <p>San Francisco, CA 94107</p>
+        <p>Site web : https://www.netlify.com</p>
+
         <Link href="/">
           <a type="button" className={`blockButton`}>Retourner au simulateur</a>
         </Link>
