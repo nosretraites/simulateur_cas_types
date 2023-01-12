25de52614ad580ed7df45b8b8a19463af1a25135
MEVEN <meven.ameline@gmail.com>
2023-01-12 19:12:28 +0100

    Warning si début de carrière avant 16 ans

--- pages/index.js
+++ pages/index.js
@@ -95,61 +95,73 @@ export default function Home() {
 
   return (
     <article>
-    <form className={styles.formElement} onSubmit={onSubmitForm}>
-      <h2>Quel effet aurait la retraite à 65 ans (réforme Macron) ?</h2>
-      <br />
-      <div className={styles.inputElement}>
-        <label htmlFor="birthDate">Année de naissance</label>
-        <input id="birthDate" min="1900" max="2022" required type="number" placeholder="1970" value={birthDate} onChange={handleNaissanceChange} />
-      </div>
-
-      <div className={styles.inputElement}>
-        <label htmlFor="careerStartAge">Âge de début de carrière</label>
-        <input id="careerStartAge" required min="12" max="40" type="number" value={careerStartAge} onChange={handleCareerStartAgeChange} />
-      </div>
-
-      <div className={styles.inputElement}>
-        <label htmlFor="yearOfCareerInterruption">Nombre d&lsquo;années non validées</label>
-        <input id="yearOfCareerInterruption" required min="0" max="40" step="0.25" type="number" value={yearOfCareerInterruption} onChange={handleCareerInterruptionChange} />
-      </div>
-
-      <div className={styles.inputElement}>
-        <label htmlFor="numberOfChildren">Nombre d&lsquo;enfants</label>
-        <input id="numberOfChildren" required min="0" max="20" type="number" value={numberOfChildren} onChange={handleNumberOfChildrenChange} />
-      </div>
-
-      {numberOfChildren > 0 ?
-        <fieldset className={styles.fieldsetElement} required onChange={handleMainParentChange}>
-          <legend>Vous êtes le parent qui a accouché de vos enfants ou pris le plus long congé d&apos;adoption ?</legend>
-          <input type="radio" id="isMainParent-true" defaultChecked={isMainParent == "true"} name="isMainParent" value="true"></input>
-          <label htmlFor="isPublicCareer-true">Oui</label>
-          <input type="radio" id="isMainParent-false" defaultChecked={isMainParent == "false"} name="isMainParent" value="false"></input>
-          <label htmlFor="isMainParent-false">Non</label>
-        </fieldset>
-        : ""}
-      {isMainParent === 'true' ?
-        <fieldset className={styles.fieldsetElement} required onChange={handlePublicCareerChange}>
-          <legend>Êtes vous employé de la Fonction publique ?</legend>
-          <input type="radio" id="isPublicCareer-true" defaultChecked={isPublicCareer == "true"} name="isPublicCareer" value="true"></input>
-          <label htmlFor="isPublicCareer-true">Public (Etat)</label>
-          <input type="radio" id="isPublicCareer-false" defaultChecked={isPublicCareer == "false"} name="isPublicCareer" value="false"></input>
-          <label htmlFor="isPublicCareer-false">Privé</label>
-        </fieldset>
-        : ""}
-
-      {numberOfChildren > 0 && isMainParent === 'true' && isPublicCareer === 'true' ?
+      <form className={styles.formElement} onSubmit={onSubmitForm}>
+        <h2>Quel effet aurait la retraite à 65 ans (réforme Macron) ?</h2>
+        <br />
         <div className={styles.inputElement}>
-          <label htmlFor="children2004">Nombre d&lsquo;enfants nés avant 2004</label>
-          <input id="children2004" required min="0" max={numberOfChildren} type="number" value={countOfChildrenBefore2004} onChange={handleChild2004Change} />
+          <label htmlFor="birthDate">Année de naissance</label>
+          <input id="birthDate" min="1900" max="2022" required type="number" placeholder="1970" value={birthDate} onChange={handleNaissanceChange} />
         </div>
-        : ""}
 
-      <button className={`blockButton ${styles.submitButton}`} type="submit">Je calcule l&apos;effet de la réforme</button>
+        <div className={styles.inputElement}>
+          <label htmlFor="careerStartAge">Âge de début de carrière</label>
+          <input id="careerStartAge" required min="16" max="40" type="number" value={careerStartAge} onChange={handleCareerStartAgeChange} />
+        </div>
 
-      <Link href="/informations">
-        <a className={`inlineButton ${styles.informationButton}`} >Plus d&apos;informations sur le simulateur</a>
-      </Link>
-    </form>
+        {careerStartAge < 16 ?
+          <div className={styles.warning}>
+            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-alert-circle" width="1em" height="em" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+              <circle cx="12" cy="12" r="9"></circle>
+              <line x1="12" y1="8" x2="12" y2="12"></line>
+              <line x1="12" y1="16" x2="12.01" y2="16"></line>
+            </svg>
+            <span>Pour l'instant, ce simulateur ne prend pas en compte les débuts de carrière avant 16 ans.</span>
+          </div>
+          : <></>
+        }
+        <div className={styles.inputElement}>
+          <label htmlFor="yearOfCareerInterruption">Nombre d&lsquo;années non validées</label>
+          <input id="yearOfCareerInterruption" required min="0" max="40" step="0.25" type="number" value={yearOfCareerInterruption} onChange={handleCareerInterruptionChange} />
+        </div>
+
+        <div className={styles.inputElement}>
+          <label htmlFor="numberOfChildren">Nombre d&lsquo;enfants</label>
+          <input id="numberOfChildren" required min="0" max="20" type="number" value={numberOfChildren} onChange={handleNumberOfChildrenChange} />
+        </div>
+
+        {numberOfChildren > 0 ?
+          <fieldset className={styles.fieldsetElement} required onChange={handleMainParentChange}>
+            <legend>Vous êtes le parent qui a accouché de vos enfants ou pris le plus long congé d&apos;adoption ?</legend>
+            <input type="radio" id="isMainParent-true" defaultChecked={isMainParent == "true"} name="isMainParent" value="true"></input>
+            <label htmlFor="isPublicCareer-true">Oui</label>
+            <input type="radio" id="isMainParent-false" defaultChecked={isMainParent == "false"} name="isMainParent" value="false"></input>
+            <label htmlFor="isMainParent-false">Non</label>
+          </fieldset>
+          : ""}
+        {isMainParent === 'true' ?
+          <fieldset className={styles.fieldsetElement} required onChange={handlePublicCareerChange}>
+            <legend>Êtes vous employé de la Fonction publique ?</legend>
+            <input type="radio" id="isPublicCareer-true" defaultChecked={isPublicCareer == "true"} name="isPublicCareer" value="true"></input>
+            <label htmlFor="isPublicCareer-true">Public (Etat)</label>
+            <input type="radio" id="isPublicCareer-false" defaultChecked={isPublicCareer == "false"} name="isPublicCareer" value="false"></input>
+            <label htmlFor="isPublicCareer-false">Privé</label>
+          </fieldset>
+          : ""}
+
+        {numberOfChildren > 0 && isMainParent === 'true' && isPublicCareer === 'true' ?
+          <div className={styles.inputElement}>
+            <label htmlFor="children2004">Nombre d&lsquo;enfants nés avant 2004</label>
+            <input id="children2004" required min="0" max={numberOfChildren} type="number" value={countOfChildrenBefore2004} onChange={handleChild2004Change} />
+          </div>
+          : ""}
+
+        <button className={`blockButton ${styles.submitButton}`} type="submit">Je calcule l&apos;effet de la réforme</button>
+
+        <Link href="/informations">
+          <a className={`inlineButton ${styles.informationButton}`} >Plus d&apos;informations sur le simulateur</a>
+        </Link>
+      </form>
     </article>
   )
 }
