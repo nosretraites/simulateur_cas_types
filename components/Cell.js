26de7635799fbfcbe8224d3a5e21b77b9b755052
MEVEN <meven.ameline@gmail.com>
2023-01-16 19:36:53 +0100

    Ajout cellule tableau orange

--- components/Cell.js
+++ components/Cell.js
@@ -15,10 +15,14 @@ export default function Cell({ data }) {
 
   useEffect(() => {
     // Different types of cell content
-    const noRetirment = <div className={styles.RedMark}><span>❌</span><span> Retraite interdite</span></div>;
-    const allowRetirment = <div className={styles.GreenMark}><span>✅</span><span> Taux plein</span></div>;
-    const decoteMessage = (value) => <div className={styles.GreenMark}><span>✅</span><span> Decote -{displayTwoDecimalsMax(value)}%</span></div>;
-    const surcoteMessage = (value) => <div className={styles.GreenMark}><span>✅</span><span> Surcote +{displayTwoDecimalsMax(value)}%</span></div>;
+
+
+    const XMarkSvg = <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-circle-x" width="1em" height="1em" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">    <path stroke="none" d="M0 0h24v24H0z" fill="none" />    <circle cx="12" cy="12" r="9" />    <path d="M10 10l4 4m0 -4l-4 4" />  </svg>;
+    const checkMarkSvg = <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-circle-check" width="1em" height="1em" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">    <path stroke="none" d="M0 0h24v24H0z" fill="none" />    <circle cx="12" cy="12" r="9" />    <path d="M9 12l2 2l4 -4" />  </svg>;
+    const noRetirment = <div className={styles.RedMark}><span>{XMarkSvg}</span><span> Retraite interdite</span></div>;
+    const allowRetirment = (color = "GreenMark") => <div className={styles[color]}><span>{checkMarkSvg}</span><span> Taux plein</span></div>;
+    const decoteMessage = (value, color = "GreenMark") => <div className={styles[color]}><span>{checkMarkSvg}</span><span> Decote -{displayTwoDecimalsMax(value)}%</span></div>;
+    const surcoteMessage = (value, color = "GreenMark") => <div className={styles[color]}><span>{checkMarkSvg}</span><span> Surcote +{displayTwoDecimalsMax(value)}%</span></div>;
 
     // Init age text
     setAge(`${data.AgeLiq} ans`)
@@ -26,18 +30,33 @@ export default function Cell({ data }) {
     const dataToDisplay = computeData(data);
 
     // Get the corresponding cell content for a specific reform
-    function getReformCellContent(reformName){
+    function getReformCellContent(reformName) {
       if (dataToDisplay[reformName].isPossible) {
         if (dataToDisplay[reformName].decote !== 0 && dataToDisplay[reformName].decote) {
-          return decoteMessage(dataToDisplay[reformName].decote);
+          let color = "GreenMark";
+          // Orange color if the value is less beneficial
+          if (reformName === 'macron' && dataToDisplay.macron.decote > dataToDisplay.base.decote) {
+            color = 'OrangeMark';
+          }
+          return decoteMessage(dataToDisplay[reformName].decote, color);
         }
-  
+
         if (dataToDisplay[reformName].surcote !== 0 && dataToDisplay[reformName].surcote) {
-          return surcoteMessage(dataToDisplay[reformName].surcote);
+          let color = "GreenMark";
+          // Orange color if the value is less beneficial
+          if (reformName === 'macron' && dataToDisplay.macron.surcote < dataToDisplay.base.surcote) {
+            color = 'OrangeMark';
+          }
+          return surcoteMessage(dataToDisplay[reformName].surcote, color);
         }
-  
+
         if (dataToDisplay[reformName].isTauxPlein) {
-          return allowRetirment
+          let color = "GreenMark";
+          // Orange color if the value is less beneficial
+          if (reformName === 'macron' && dataToDisplay.base.surcote) {
+            color = 'OrangeMark';
+          }
+          return allowRetirment(color)
         }
       } else {
         return noRetirment
